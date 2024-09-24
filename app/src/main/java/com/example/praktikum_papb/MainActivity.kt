314a5bb817cb6dfacc1d996d99572934d4572067
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2024-09-24 09:06:19 +0700

    add modul 4

--- app/src/main/java/com/example/praktikum_papb/MainActivity.kt
+++ app/src/main/java/com/example/praktikum_papb/MainActivity.kt
@@ -5,13 +5,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import com.example.praktikum_papb.ui.theme.PraktikumPAPBTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,20 +41,85 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PraktikumPAPBTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var nama by remember { mutableStateOf("") }
+    var NIM by remember { mutableStateOf("") }
+    var showInput by remember { mutableStateOf("") }
+    val buttonColor = if (nama.isNotEmpty() && NIM.isNotEmpty() && NIM.isDigitsOnly()) Color.Blue else Color.Gray
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = showInput, modifier = Modifier.padding(bottom = 16.dp))
+        OutlinedTextField(
+            value = nama,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "PersonIcon"
+                )
+            },
+            onValueChange = { nama = it },
+            label = { Text("Input Your Name") },
+            placeholder = { Text(text = "Enter Your Name") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            shape = RoundedCornerShape(8.dp),
+        )
+
+        OutlinedTextField(
+            value = NIM,
+            leadingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = "NIMIcon") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { NIM = it },
+            label = { Text("Input Your NIM") },
+            placeholder = { Text(text = "Enter Your NIM") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            shape = RoundedCornerShape(8.dp),
+        )
+
+        Button(
+            onClick = {
+                if (nama.isNotEmpty() && NIM.isNotEmpty() && NIM.isDigitsOnly()) {
+                    showInput = "Name : $nama\nNIM    : $NIM"
+                }
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
+            shape = RoundedCornerShape(20.dp),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 15.dp,
+                disabledElevation = 0.dp
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text("Show Name and NIM", color = Color.White)
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     PraktikumPAPBTheme {
+        MainScreen()
     }
 }
